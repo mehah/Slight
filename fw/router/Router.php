4299942244a03719a6f47fe7e75aaeac6e5a8879
mehah <mehahx@gmail.com>
2018-06-23 13:41:10 -0300

    incresed performance on router

--- fw/router/Router.php
+++ fw/router/Router.php
@@ -6,30 +6,30 @@ final class Router {
 	private static $list = [];
 
 	public static function get(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('GET', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('GET', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
 	public static function post(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('POST', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('POST', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
 	public static function put(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('PUT', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('PUT', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
 	public static function delete(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('DELETE', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('DELETE', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
 	public static function patch(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('PATCH', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('PATCH', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
 	public static function options(string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
-		self::registerController('OPTIONS', $urlPath, $controllerClass, $methodName, $accessRule);
+		self::register('OPTIONS', $urlPath, $controllerClass, $methodName, $accessRule);
 	}
 
-	private static function registerController(string $methodType, string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
+	private static function register(string $methodType, string $urlPath, string $controllerClass, string $methodName, array $accessRule = null): void {
 		if (filter_var('http://' . $urlPath, FILTER_VALIDATE_URL) === false) {
 			throw new \Exception('Invalid URL: ' . $urlPath);
 		}
@@ -50,32 +50,41 @@ final class Router {
 			self::$list[$methodType] = &$routerList;
 		}
 		
-		foreach (explode('/', $urlPath) as $i => $value) {
-			if (! $value) {
-				continue;
-			}
-			
-			if ($value[0] === ':') {
-				if ($i === 0) {
-					throw new \Exception('First character can not be a parameter.');
+		if (strpos($urlPath, ':') === false) {
+			$routerList[$urlPath] = [];
+			$routerList = &$routerList[$urlPath];
+		} else {
+			foreach (explode('/', $urlPath) as $i => $value) {
+				if (! $value) {
+					continue;
 				}
 				
-				$name = substr($value, 1);
-				
-				if (isset($routerList['$param']) && $routerList['$param']['name'] !== $name) {
-					throw new \Exception('It is not possible to register a URL with a parameter name different from one already registered.');
+				if ($value[0] === ':') {
+					if ($i === 0) {
+						throw new \Exception('First character can not be a parameter.');
+					}
+					
+					$name = substr($value, 1);
+					
+					if (isset($routerList['$param']) && $routerList['$param']['name'] !== $name) {
+						throw new \Exception('It is not possible to register a URL with a parameter name different from one already registered.');
+					}
+					
+					$routerList['$param']['name'] = $name;
+					$routerList = &$routerList['$param'];
+					continue;
 				}
 				
-				$routerList['$param']['name'] = $name;
-				$routerList = &$routerList['$param'];
-				continue;
+				if (! isset($routerList[$value])) {
+					$routerList[$value] = [];
+				}
+				
+				$routerList = &$routerList[$value];
 			}
-			
-			if (! isset($routerList[$value])) {
-				$routerList[$value] = [];
-			}
-			
-			$routerList = &$routerList[$value];
+		}
+		
+		if (isset($routerList['config'])) {
+			throw new \Exception("There is already a routing with the URL '$urlPath', pointing to controller('$controllerClass') and method('$methodName').");
 		}
 		
 		$routerList['config'] = [
@@ -91,15 +100,19 @@ final class Router {
 			return null;
 		}
 		
-		foreach (explode('/', $url) as $value) {
-			if (! $value) {
-				continue;
-			}
-			
-			if (isset($router[$value])) {
-				$router = $router[$value];
-			} elseif ($router = $router['$param'] ?? null) {
-				$_REQUEST[$router['name']] = $value;
+		if (isset($router[$url])) {
+			$router = $router[$url];
+		} else {
+			foreach (explode('/', $url) as $value) {
+				if (! $value) {
+					continue;
+				}
+				
+				if (isset($router[$value])) {
+					$router = $router[$value];
+				} elseif ($router = $router['$param'] ?? null) {
+					$_REQUEST[$router['name']] = $value;
+				}
 			}
 		}
 		
